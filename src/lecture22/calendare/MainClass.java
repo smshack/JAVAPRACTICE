142ac2f5631673662c36815df35ed0e7ff7d0bb4
smshack <5432tat@naver.com>
2020-08-28 02:01:25 +0900

    23. API2 *Random 클래스 임의의 수를 발생 시키는 클래스 예전에는 Math.random()을 많이 이용하였으나, 요즘은 Random 클래스를 많이 사용
    Math.random 메소드는  double 타입의 임의의 수를 발생
    static이고 반환 타입이 doucle
    
    
    
    *Scanner 및 Sysout 클래스
    *Wrapper 클래스
    자료형 변환
    기초데이터를 객체 데이터로 변환
    기초데이터		객체데이터
    byte		Byte
    short		Short
    int			Integer
    long 		Long
    double		Double
    float		Float
    boolean		Boolean
    char		Character

--- src/lecture22/calendare/MainClass.java
+++ src/lecture22/calendare/MainClass.java
@@ -24,7 +24,7 @@ public class MainClass {
 		System.out.println(year + "년 " + month + "월 " + day + "일");
 		System.out.println(hour + "시 " + minute + "분 " + second + "초");
 		System.out.println();
-
+		System.out.println("---------------------------------------------------------------");
 		System.out.println(System.currentTimeMillis());
 
 		//String와 StringBuilder의 성능 차이 비교
@@ -47,5 +47,12 @@ public class MainClass {
 
 		long endNum1 = System.currentTimeMillis();
 		System.out.println("경과 시간" + (endNum1 - startNum1));
+		System.out.println("---------------------------------------------------------------");
+		
+		
+		
+		
+		
+		
 	}
 }
